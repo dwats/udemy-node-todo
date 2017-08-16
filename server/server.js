c961326975b4b46f62901097a6fed296a8e11e37
Eric Julius <ejulius86@gmail.com>
2017-08-16 01:11:53 -0500

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  var todo = new Todo({
+  const todo = new Todo({
     text: req.body.text
   });
 
@@ -30,8 +30,8 @@ app.get('/todos', (req, res) => {
       res.send({ todos });
     }, (e) => {
       res.status(400).send(e);
-    })
-})
+    });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
