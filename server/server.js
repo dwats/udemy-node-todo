af3f6b6c3b7ca9d096954fc140df78577ee45908
Eric Julius <ejulius86@gmail.com>
2017-08-24 18:50:02 -0500

    Added POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -88,6 +88,21 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user
+    .save()
+    .then(() => user.generateAuthToken())
+    .then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e.message);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Started at port ${port}`);
 });
